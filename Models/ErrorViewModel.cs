192503300f6986118306a02a894977847c1bee6f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-18 20:24:22 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BriggsdaleUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
